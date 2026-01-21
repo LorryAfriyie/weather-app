ebe13c2dad25510eb62085674a3a75542cbcc88d
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2026-01-21 19:47:32 +0200

    Add search input and button to weather dashboard
    Introduced a search input field and button to the WeatherDashboard component for user interaction. Updated SCSS to style the new input and button, improving the dashboard's UI and usability.

--- src/components/weatherDashboard.tsx
+++ src/components/weatherDashboard.tsx
@@ -1,7 +1,19 @@
 const WeatherDashboard = () => {
   return (
     <section className="weather-dashboard">
-      <h1>weather dashboard</h1>
+      <h1>How's the sky looking today?</h1>
+
+      <div className="input-container">
+        <input
+          type="text"
+          className="search-input"
+          id={"search-input"}
+          name={"search-input"}
+        />
+        <button className={"search-btn"} id={"search-btn"} name={"search-btn"}>
+          Search
+        </button>
+      </div>
     </section>
   );
 };
