c47c450481e7eb35ae9bd76cdac5cd1337107e39
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2026-03-06 19:46:49 +0200

    Internet connection error component
    Created a new component that displays an error message when there is issue connecting to the weather server.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { Header } from "./components/header.tsx";
+import { ErrorIcon } from "./components/svg.tsx";
 import WeatherDashboard from "./components/weatherDashboard";
 
 function App() {
   return (
     <div className={"app"}>
       <Header />
-      <WeatherDashboard />
+      <NoConnectionMsg />
+      {/*<WeatherDashboard />*/}
     </div>
   );
 }
 
+function NoConnectionMsg() {
+  return (
+    <div className={"connection-container"}>
+      <ErrorIcon />
+      <h2>Something went wrong</h2>
+      <p>
+        We couldn't connect to the server (API error). Please try again in a few
+        moments.
+      </p>
+
+      <button>Retry</button>
+    </div>
+  );
+}
 export default App;
